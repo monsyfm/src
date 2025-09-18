1b0761b99fad8a426ed1db352cf3eb5ec86e80ae
monsyfm <monsyfm@gmail.com>
2025-09-18 11:57:30 -0600

    Add files via upload

---
+++ CONDICIONAL.C
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int var1 = 0; 
+    int var2;
+
+    if (var1 == 0) {
+        var2 = 1;
+    } else {
+        var2 = -1;
+    }
+
+    printf("Resultado: %d\n", var2);
+    return 0;
+}
