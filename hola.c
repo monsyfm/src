8bf73e7ff4ce8c74b848d200cb4c395f3fd662f6
monsyfm <monsyfm@gmail.com>
2025-09-19 12:34:12 -0600

    cambio de codigo

--- hola.c
+++ hola.c
@@ -3,6 +3,6 @@
 int main() {
     printf("Hola Mundo carmen monterrat flores medina\n");  // Imprime el mensaje en la consola
      printf("HOLA\n");
-      printf("Hola Mundo carmen monterrat flores medina\n");
+      printf("CAMBIO PARA LA SEGUNDA VERSIÓN \n");
     return 0;                // Indica que el programa terminó correctamente
 }
